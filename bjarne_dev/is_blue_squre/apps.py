e2aecbf4aea0fcb1e4c8e94cda2480d4371202f6
bj4rnee <bjarne.klar@web.de>
2024-09-11 18:09:04 +0200

    init more apps

---
+++ bjarne_dev/is_blue_squre/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IsBlueSqureConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'is_blue_squre'
---
+++ readme.md
@@ -0,0 +1,3 @@
+# https://bjarne.dev
+
+This is the repository of my personal homepage [bjarne.dev](https://bjarne.dev).
